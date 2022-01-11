25fc82178a080e3e442cc2bf933384133af6b8c5
tw-yshuang <tw-yshuang@gmail.com>
2022-01-11 20:01:29 +0800

    [add][mod] final project stage 1 finish.

--- Server/backend.py
+++ Server/backend.py
@@ -1,4 +1,6 @@
 import os, sys, shutil, time
+import numpy as np
+from datetime import datetime
 from flask import Flask, app, render_template, request, jsonify
 from paho.mqtt.client import Client
 from pyecharts import options as opts
@@ -9,18 +11,22 @@ from jinja2 import Markup
 
 
 from src.mqtt import get_mqtt_client
-from src.db_tool import insert_db
+from src.db_tool import insert_db, select_db
 
 app = Flask(__name__, static_folder="templates")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global CHART_DISPLAY, CHART_DAYS
     # if request.method == 'GET':
     #     lightON = True if 'true' == request.args.get('lightON', default=None, type=str) else False
     # print(request.args.get('lightON', default=None, type=bool))
-    # if request.method == 'POST':
-    #     print(request.args.get('isSwitch', default=None, type=bool))
+    if request.method == 'POST':
+        CHART_DISPLAY = request.form.get('sensor', type=str)
+        CHART_DAYS = request.form.get('days', type=int)
+        print(request.form.get('days', type=int))
+        get_dayLine(CHART_DISPLAY, CHART_DAYS)
     # sleep(5)
     # trans_controller()
 
@@ -51,6 +57,7 @@ def trans_controller():
 
 @app.route("/barChart")
 def get_barChart():
+
     c = (
         Bar()
         .add_xaxis(["00:00~04:00", "04:00~8:00", "08:00~12:00", "12:00~16:00", "16:00~20:00", "20:00~00:00"])
@@ -65,6 +72,79 @@ def get_barChart():
 
     return c.dump_options_with_quotes()
     # return a
+
+
+@app.route("/dayLine")
+def get_dayLine(sensor=None, days=3):
+    global CHART_DISPLAY, CHART_DAYS
+    if sensor is None:
+        sensor = CHART_DISPLAY
+        days = CHART_DAYS
+    snesor_dict = {'temperature': 0, 'humility': 1, 'illuminate': 2}
+    days_data = [data_analysis(last_day=i) for i in range(days)]
+
+    current_hr = int(datetime.fromtimestamp(time.time()).strftime("%H"))
+    c = (
+        Line()
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title=sensor),
+            tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),
+            datazoom_opts=[opts.DataZoomOpts()],
+            yaxis_opts=opts.AxisOpts(
+                type_="value",
+                axistick_opts=opts.AxisTickOpts(is_show=True),
+                splitline_opts=opts.SplitLineOpts(is_show=True),
+            ),
+            xaxis_opts=opts.AxisOpts(type_="category", boundary_gap=False),
+        )
+        .add_xaxis([(f'{current_hr + i}' if 24 - current_hr > i else f'{current_hr + i - 24}') for i in range(0, 24)])
+    )
+    for i in range(days):
+        c.add_yaxis(
+            f"{days - i} day",
+            days_data[i][snesor_dict[sensor]],
+            areastyle_opts=opts.AreaStyleOpts(opacity=0.3),
+            label_opts=opts.LabelOpts(is_show=True, position="top"),
+            is_smooth=True,
+        )
+
+    return c.dump_options_with_quotes()
+
+
+@app.route("/tab_charts")
+def get_tab_charts():
+    sensor_dict = {'temperature': 0, 'humility': 1, 'illuminate': 2}
+    days_data = [data_analysis(last_day=i) for i in range(5)]
+    current_hr = int(datetime.fromtimestamp(time.time()).strftime("%H"))
+    tab = Tab()
+    for k, v in sensor_dict.items():
+        c = (
+            Line()
+            .set_global_opts(
+                title_opts=opts.TitleOpts(title=k),
+                tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),
+                datazoom_opts=[opts.DataZoomOpts()],
+                yaxis_opts=opts.AxisOpts(
+                    type_="value",
+                    axistick_opts=opts.AxisTickOpts(is_show=True),
+                    splitline_opts=opts.SplitLineOpts(is_show=True),
+                ),
+                xaxis_opts=opts.AxisOpts(type_="category", boundary_gap=False),
+            )
+            .add_xaxis([(f'{current_hr + i}' if 24 - current_hr > i else f'{current_hr + i - 24}') for i in range(0, 24)])
+        )
+        for i in range(5):
+            c.add_yaxis(
+                f"{5 - i} day",
+                days_data[i][v],
+                areastyle_opts=opts.AreaStyleOpts(opacity=0.3),
+                label_opts=opts.LabelOpts(is_show=True, position="top"),
+                is_smooth=True,
+            )
+
+        tab.add(c, k)
+    tab.render('./templates/tab_charts.html')
+    return render_template('tab_charts.html')
 
 
 @app.route("/humility")
@@ -87,6 +167,16 @@ def get_humility(value=29.6):
     return c.dump_options_with_quotes()
 
 
+@app.route("/illuminate")
+def get_illuminate():
+    return jsonify(DEVICE_DICT[SENSOR_LIST[2]][:5])
+
+
+@app.route("/temperature")
+def get_temperature():
+    return jsonify(DEVICE_DICT[SENSOR_LIST[0]])
+
+
 def receive_edge(client, userdata, msg):
     # sensor_name = msg.topic.split('/')[-1]
 
@@ -99,6 +189,30 @@ def receive_edge(client, userdata, msg):
     print(DEVICE_DICT)
 
 
+def get_data(from_unix: time.time(), to_unix: time.time()):
+    return select_db(
+        (
+            'Timestamp',
+            f'{datetime.fromtimestamp(from_unix - 28800).strftime("%Y-%m-%d %H:%M:%S")}',
+            f'{datetime.fromtimestamp(to_unix - 28800).strftime("%Y-%m-%d %H:%M:%S")}',
+        )
+    )
+
+
+def data_analysis(last_day: int):
+    # print(f'{datetime.fromtimestamp(time.time() - 3600).strftime("%Y-%m-%d %H:%M:%S")}')
+    time_block = [[], [], []]
+    last_day += 1
+    for hr in range(0, 24):
+        source = get_data(from_unix=time.time() - (24 - hr) * last_day * 60 * 60, to_unix=time.time() - (23 - hr) * last_day * 60 * 60)
+        source = np.array(source, dtype=np.object_)
+        time_block[0].append(int(sum(source[:, 2]) / source.shape[0]))
+        time_block[1].append(int(sum(source[:, 3]) / source.shape[0]))
+        time_block[2].append(int(sum(source[:, 4]) / source.shape[0]))
+        # t_mean = sum(source[:][2]) / len(source[:][2])
+    return time_block
+
+
 def main():
     db_path = './Data/sensor-data.db'
 
@@ -106,10 +220,12 @@ def main():
         shutil.copy('./Data/default_sensor-data.db', db_path)
         print("Successfully copy file.")
 
-    global DEVICE_DICT, SENSOR_LIST, CLIENT
+    global DEVICE_DICT, SENSOR_LIST, CLIENT, CHART_DISPLAY, CHART_DAYS
     DEVICE_DICT = {}  # {light_switch: bool, fan_switch: bool}
-    SENSOR_LIST = ['Temperature', 'Humility', 'Illuminate', 'Motion']
+    SENSOR_LIST = ['temperature', 'humility', 'illuminate', 'motion']
     CLIENT = get_mqtt_client(msg_func=receive_edge)
+    CHART_DISPLAY = 'humility'
+    CHART_DAYS = 3
 
     app.run(host='0.0.0.0', debug=True, port=8000)
 
--- Server/src/db_tool.py
+++ Server/src/db_tool.py
@@ -37,16 +37,30 @@ def control_db(db_path: str = './Data/sensor-data.db'):
 
 
 def insert_db(data, db_path: str = './Data/sensor-data.db'):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
 
-    sql = f''' 
-            INSERT INTO iiot_devices (Temperature, Humility, Illuminate, Motion) 
-            VALUES ( {data[0]}, {data[1]}, {data[2]}, {data[3]}) 
-            '''
+        sql = f''' 
+                INSERT INTO iiot_devices (Temperature, Humility, Illuminate, Motion) 
+                VALUES ( {data[0]}, {data[1]}, {data[2]}, {data[3]}) 
+                '''
 
-    cursor.execute(sql)
-    conn.commit()
+        cursor.execute(sql)
+        conn.commit()
+
+
+def select_db(option: tuple, db_path: str = './Data/sensor-data.db'):
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+
+        sql = f''' 
+                SELECT * FROM iiot_devices
+                    WHERE {option[0]} >= "{option[1]}*"
+                    AND   {option[0]} <  "{option[2]}*"
+                '''
+
+        cursor.execute(sql)
+        return cursor.fetchall()
 
 
 if __name__ == '__main__':
